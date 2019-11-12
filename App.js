3d3c9ece38f65f1d411daa18bf4144171362e6ac
MateusSchulz <mateus.schulz.m@gmail.com>
2019-11-12 12:29:56 -0200

    Bug fonts

--- App.js
+++ App.js
@@ -1,35 +1,63 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
+import React, { Component } from 'react';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+
+import { Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-    };
+export default class AnatomyExample extends Component {
+
+  state = {
+    loading: true
   }
 
   async componentDidMount() {
     await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
+    })
+    this.setState({ loading: false })
   }
 
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
 
-    return (
-      <Container>
-        <Text>Open up App.js to start working on your app! :)</Text>
-      </Container>
-    );
-  } 
+  render() {
+
+    if (this.state.loading) {
+      return (
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name='menu' />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Header</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+            <Text>
+              This is Content Section
+            </Text>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button full>
+                <Text>Footer</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      );
+    } else{
+      return (
+        <View>
+            <Button>
+              <Text>Click me!</Text>
+            </Button>
+         </View>
+       )
+    }
+  }
 }
